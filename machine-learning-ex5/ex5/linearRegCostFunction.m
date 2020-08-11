6df55ea3469b359a6f784dda95e939ada1a28309
Kaiyan Li <kkyli@ucdavis.edu>
2020-08-11 07:03:50 -0700

    complete part 2

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -21,7 +21,7 @@ grad = zeros(size(theta));
 
 J = 1/(2*m)*sum((X*theta-y).^2) + lambda/(2*m)*sum(theta(2:end).^2);
 grad(1,1) = 1/m*sum((X*theta-y).*X(:,1));
-grad(2:end,1) = 1/m*sum((X*theta-y).*X(:,2:end))' + lambda/m*theta(2:end,1);
+grad(2:end,1) = 1/m*sum((X*theta-y).*X(:,2:end),1)' + lambda/m*theta(2:end,1);
 
 % =========================================================================
 
